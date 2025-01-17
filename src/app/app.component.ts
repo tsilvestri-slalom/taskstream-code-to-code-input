a316b6a8c95dbd4c39cda79dd1a2044855440862
Tony Silvestri <tonys@slalom.com>
2025-01-17 14:54:01 -0500

    Change for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Simple Angular App (Multiple File)';
-  items = ['Item 1', 'Item 2', 'Item 3'];
+  items = ['Item 1.', 'Item 2.', 'Item 3.'];
   selectedItem: string | null = null;
 
   selectItem(item: string): void {
